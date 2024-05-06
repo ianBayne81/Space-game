b6a0dd67d283cf4750d65cf4c9ad184f7fdf02ad
Ian Bayne <43926592+ianBayne81@users.noreply.github.com>
2024-05-06 10:33:14 +1000

    updates

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Canvas {
         this.canvas = document.getElementById("canvas")
         this.ctx = this.canvas.getContext("2d")
         this.width = 350
-        this.height = 500
+        this.height = 450
     } 
 }
 
@@ -15,18 +15,18 @@ class Newgame extends Canvas {
     constructor() {
         super()
         this.score = 0
-        this.bullets = 10
-        this.obstacleX = this.width / 2
+        this.bullets = 100
+        this.obstacleX = Math.floor(Math.random() * (1000 - 655))
         this.obstacleY = 0
-        this.obstacleWidth = 10
-        this.obstacleHeight = 75
-        this.obstacleDirection = 2.5
-        this.playerWidth = 50
-        this.playerHeight = -100
+        this.obstacleWidth = 25
+        this.obstacleHeight = 50
+        this.obstacleDirection = 4
+        this.playerWidth = 30
+        this.playerHeight = -50
         this.x = (this.width - this.playerWidth) / 2
         this.y = this.height - 20
-        this.dx = 50
-        this.dy = -50
+        this.dx = 20
+        this.dy = -20
         this.enemyX = this.width / 2
         this.enemyY = 0
         this.enemyRadius = 75
@@ -34,7 +34,8 @@ class Newgame extends Canvas {
         this.bulletRadius = 5
         this.bulletX = this.x + this.playerWidth / 2
         this.bulletY = this.y + this.playerHeight
-        this.bulletDirection = 5
+        this.bulletDirection = 4
+        this.padding = 15
     }
 
     drawScore() {
@@ -46,14 +47,23 @@ class Newgame extends Canvas {
     drawAmmunition() {
         this.ctx.font = "18px Arial"
         this.ctx.fillStyle = "white"
-        this.ctx.fillText(`Ammo: ${this.bullets}`, this.canvas.width - 90, 20)
+        this.ctx.fillText(`Ammo: ${this.bullets}`, this.canvas.width - 100, 20)
     }
-
+    
     drawObstacle() {
         this.ctx.beginPath()
-        this.ctx.rect(this.obstacleX - (this.obstacleWidth / 2), this.obstacleY, this.obstacleWidth, this.obstacleHeight)
+        this.ctx.rect(this.obstacleX, this.obstacleY, this.obstacleWidth, this.obstacleHeight)
         this.ctx.fillStyle = "lime"
         this.ctx.fill()
+
+        if (this.obstacleY > this.height) {
+            this.obstacleY = 0
+            this.obstacleX = Math.floor(Math.random() * (1000 - 655))
+            this.drawObstacle()
+            
+        }
+
+        this.obstacleY += this.obstacleDirection
     }
 
     drawPlayer() {
@@ -81,10 +91,48 @@ class Newgame extends Canvas {
         this.ctx.fill()
         this.ctx.closePath()
         
+        if (this.bulletY === 0) {
+            this.bulletX = this.x + this.playerWidth / 2
+            this.bulletY = this.y + this.playerHeight
+        }
+
         this.bulletY -= this.bulletDirection
-        
     }
 
+    playerBeingHit() {
+        if (this.obstacleY + this.obstacleHeight + this.obstacleDirection >= this.y + this.playerHeight &&
+            this.obstacleX + this.obstacleWidth >= this.x &&
+            this.obstacleX <= this.x + this.playerWidth) {
+                this.obstacleDirection = 0
+                alert("Game over, you've been hit!")
+                location.reload()
+        }
+    }
+    
+    enemyBeingHit() {
+        if (this.bulletX  > this.enemyX - this.enemyRadius &&
+            this.bulletX < this.enemyX + this.enemyRadius &&
+            this.bulletY <= this.enemyY + this.enemyRadius) {
+
+            this.score += 1
+            this.enemyHits++
+            this.bulletX = this.x + this.playerWidth / 2
+            this.bulletY = this.y + this.playerHeight
+        }
+    }
+
+    bulletCollision() {
+        if (this.bulletY - this.bulletRadius < this.obstacleY + this.obstacleHeight &&
+            this.bulletX >= this.obstacleX &&
+            this.bulletX <= this.obstacleX + this.obstacleWidth) {
+                this.score++
+                this.obstacleY = 0
+                this.obstacleX = Math.floor(Math.random() * (1000 - 655))
+                this.bulletX = this.x + this.playerWidth / 2
+                this.bulletY = this.y + this.playerHeight
+        }
+    }
+    
     render() {
         this.ctx.clearRect(0, 0, this.width, this.height)
         this.drawScore()
@@ -92,65 +140,20 @@ class Newgame extends Canvas {
         this.drawObstacle()
         this.drawPlayer()
         this.drawEnemy()
+        this.playerBeingHit()
+        this.enemyBeingHit()
+        this.bulletCollision()
         this.drawBullet()
-    
-        this.obstacleY += this.obstacleDirection
-    
-        //repeat draw obstacle randomly
-        if (this.obstacleY > this.height) {
-            this.score++
-            this.obstacleY = 0
-            this.obstacleX = Math.random() * (1000 - 655)
-            this.drawObstacle()
-            this.obstacleY += this.obstacleDirection
-        }
-    
-        //collision detection of player being hit by obstacle
-        if (this.obstacleY + this.obstacleHeight + this.obstacleDirection > (this.y + this.playerHeight) - this.obstacleDirection &&
-            this.obstacleX > this.x &&
-            this.obstacleX < this.x + this.playerWidth ) {
-                this.obstacleDirection = 0
-                alert("Game over, you've been hit!")
-                location.reload()
-        }
-    
-        //hitting the enemy with bullets
-        if (this.bulletX  > this.enemyX - this.enemyRadius &&
-            this.bulletX < this.enemyX + this.enemyRadius &&
-            this.bulletY + this.bulletDirection === this.enemyY) {
-            
-            this.score += 2
-            this.enemyHits++
-            console.log(this.enemyHits)
-            
-        }
-
-        //bullet collides with obstacle
-        if (this.bulletX + this.bulletRadius >= this.obstacleX && 
-            this.bulletX - this.bulletRadius <= this.obstacleX + this.obstacleWidth && 
-            this.bulletY + this.bulletRadius + this.bulletDirection === this.obstacleY + this.obstacleHeight) {
-                this.score++
-                this.obstacleY = 0
-                this.obstacleX = Math.random() * (1000 - 655)
-                this.drawObstacle()
-                this.obstacleY += this.obstacleDirection
-        }
-
-        requestAnimationFrame(() => this.render())
         
+        requestAnimationFrame(() => this.render())
     }
-    
 }
-
-
 
 //eventlisteners for the buttons
 document.querySelector("#start").addEventListener("click", function (e) {
     e.preventDefault()
     start = new Newgame
     start.render()
-    requestAnimationFrame(() => start.render())
-    
 })
 
 document.querySelector("#forward").addEventListener("click", function (e) {
@@ -190,21 +193,16 @@ document.querySelector("#right").addEventListener("click", function (e) {
     start.x += start.dx
 })
 
-
-
 document.querySelector("#fire").addEventListener("click", function (e) {
     e.preventDefault()
-    //shoot bullet
-
     if (start.bullets > 0) {
         start.bulletX = start.x + start.playerWidth / 2
         start.bulletY = start.y + start.playerHeight
+        start.drawBullet()
         start.bullets--
     } else {
         alert("You have no bullets left")
     }
-    
-    requestAnimationFrame(() => start.drawBullet())
     
 })
 
